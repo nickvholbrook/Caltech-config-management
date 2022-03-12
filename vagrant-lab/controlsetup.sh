43f98383947a4998df0686057743b180bdea2cdb
nickvholbrook <nick.holbrook@gmail.com>
2022-03-12 16:01:19 +0000

    Added bootstrap to control

--- vagrant-lab/controlsetup.sh
+++ vagrant-lab/controlsetup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt update -y
-useradd -m devops -p "p@ssw0rd1234"
-ssh-keygen -t rsa -f /home/devops/.ssh/id_rsa -q -P ""
+useradd -m devops -p "password1234"
+
 usermod -a -G sudo devops
-#sudo sh -c "echo \"group ALL=(user) NOPASSWD: ALL\" >> /etc/sudoers"
 sed -i '21 i devops  ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
-ssh-copy-id devops@192.168.56.6
+
+# Create and copy over public key
+#ssh-keygen -t rsa -f /home/devops/.ssh/id_rsa -q -P ""  # Run as devops user
+#ssh-copy-id devops@192.168.56.6
