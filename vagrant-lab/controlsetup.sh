27f282e5e6fab546f5f95aa8e303e43d84f2837f
nickvholbrook <nick.holbrook@gmail.com>
2022-03-12 15:37:14 +0000

    Upated Ubuntu to 21.04

--- vagrant-lab/controlsetup.sh
+++ vagrant-lab/controlsetup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-useradd -m devops -p P@ssw0rd1234
+apt update -y
+useradd -m devops -p "p@ssw0rd1234"
 ssh-keygen -t rsa -f /home/devops/.ssh/id_rsa -q -P ""
 usermod -a -G sudo devops
-sudo sh -c "echo \"group ALL=(user) NOPASSWD: ALL\" >> /etc/sudoers"
+#sudo sh -c "echo \"group ALL=(user) NOPASSWD: ALL\" >> /etc/sudoers"
+sed -i '21 i devops  ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
+ssh-copy-id devops@192.168.56.6
