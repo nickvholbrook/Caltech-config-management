7db01c922328f2d24c91eaad4b1b5b91ad8275f4
nickvholbrook <nick.holbrook@gmail.com>
2022-03-12 18:01:15 +0000

    Reordered files

--- vagrant-lab/controlsetup.sh
+++ vagrant-lab/controlsetup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 apt update -y
-useradd -m devops -p "password1234"
 
+useradd -m devops -p "password1234"
 usermod -a -G sudo devops
 sed -i '21 i devops  ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
 
-sudo apt update 
-sudo apt install software-properties-common 
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible -y
+sudo apt install ansible -y
 ansible --version
 
-# Create and copy over public key
-#ssh-keygen -t rsa -f /home/devops/.ssh/id_rsa -q -P ""  # Run as devops user
-#ssh-copy-id devops@192.168.56.6
+
+# Run on web and DB.  Create and copy over public key
+useradd -m devops -p "password1234"
+usermod -a -G sudo devops
+sed -i '21 i devops  ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
+
+# Run from control host
+# ssh-keygen -t rsa -f /home/devops/.ssh/id_rsa -q -P ""  # Run as devops user
+# ssh-copy-id devops@192.168.56.6
